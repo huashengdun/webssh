760c74a2f7c7227ac0df30abd879254b8118642d
Sheng <webmaster0115@gmail.com>
2020-10-02 11:29:48 +0800

    Use selector event loop for Python 3.8+ on windows

--- webssh/__init__.py
+++ webssh/__init__.py
@@ -1,4 +1,11 @@
+import sys
 from webssh._version import __version__, __version_info__
 
 
 __author__ = 'Shengdun Hua <webmaster0115@gmail.com>'
+
+
+if sys.platform == 'win32' and sys.version_info.major == 3 and \
+        sys.version_info.minor >= 8:
+    import asyncio
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
