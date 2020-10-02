063b0ee5cf76fbbd3470111837104d9e233face8
Sheng <webmaster0115@gmail.com>
2020-10-02 13:55:56 +0800

    Bump version to 1.5.3

--- webssh/__init__.py
+++ webssh/__init__.py
@@ -4,7 +4,6 @@ from webssh._version import __version__, __version_info__
 
 __author__ = 'Shengdun Hua <webmaster0115@gmail.com>'
 
-
 if sys.platform == 'win32' and sys.version_info.major == 3 and \
         sys.version_info.minor >= 8:
     import asyncio
