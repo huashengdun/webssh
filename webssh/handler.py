3639dcc33a3c9387420e0ce0a94db618bdbc38b7
masc0 <masc@c138.de>
2021-07-19 16:47:17 +0200

    Fix Passphrase Disclosure

--- webssh/handler.py
+++ webssh/handler.py
@@ -178,8 +178,7 @@ class PrivateKey(object):
         logging.error(str(self.last_exception))
         msg = 'Invalid key'
         if self.password:
-            msg += ' or wrong passphrase "{}" for decrypting it.'.format(
-                    self.password)
+            msg += ' or wrong passphrase for decrypting it.'
         raise InvalidValueError(msg)
 
 
