97e6d2555629477e120643dc5d4d0fe6a66de1cc
Sheng <webmaster0115@gmail.com>
2022-05-02 19:58:38 +0800

    Support Python 3.9

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     install_requires=[
         'tornado>=4.5.0',
