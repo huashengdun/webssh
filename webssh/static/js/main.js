73158d0b4a84ad3cdf729b03dcd5632b6496664d
npoh 💻 <np1810@pm.me>
2024-02-05 21:22:50 +0530

    Added xterm addons webgl 0.16.0, web links 0.9.0

--- webssh/static/js/main.js
+++ webssh/static/js/main.js
@@ -385,6 +385,10 @@ jQuery(function($){
 
     term.fitAddon = new window.FitAddon.FitAddon();
     term.loadAddon(term.fitAddon);
+    term.WebglAddon = new window.WebglAddon.WebglAddon();
+    term.loadAddon(term.WebglAddon);
+    term.WebLinksAddon = new window.WebLinksAddon.WebLinksAddon();
+    term.loadAddon(term.WebLinksAddon);
 
     console.log(url);
     if (!msg.encoding) {
@@ -549,6 +553,7 @@ jQuery(function($){
     };
 
     sock.onclose = function(e) {
+      term.WebglAddon.dispose();
       term.dispose();
       term = undefined;
       sock = undefined;
