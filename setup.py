e83f233eee0b3f6b48f467862a93f64dac72ce6d
Sheng <webmaster0115@gmail.com>
2025-07-27 18:58:09 +0800

    Updated version

--- setup.py
+++ setup.py
@@ -25,10 +25,9 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     install_requires=[
         'tornado>=4.5.0',
